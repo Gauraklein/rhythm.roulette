6ecac863e3d6c6ba1ff506a4e9f5a2e28121dfb7
gauraklein <gaurakklein@gmail.com>
2021-05-30 11:57:02 -0500

    basic randomizer functionality working, need to refine api call, deal with edge case and focus on UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,22 @@
 import * as React from "react"
 
 import { Get } from 'react-axios'
-
+import ReactPlayer from 'react-player'
+import randomDates from "../components/date-generator"
 
 const IndexPage = () => {
-  // the guts of the randomization will be balancing these 2 values
-  // they are expected to be in RFC 3339 formatted date-time value (1970-01-01T00:00:00Z).
-  // may need dayjs or could do some Math.random shenanigans
 
   // TODO: Externalize api calls and randomizer
 
-  let publishedAfter = "2020-12-01T00:00:00Z"
+  const rDates = randomDates()
 
-  let publishedBefore = "2021-01-01T00:00:00Z"
+  const ytUrl = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=vinyl&publishedAfter=" + rDates.publishedAfter + "&publishedBefore=" + rDates.publishedBefore + "&type=video&order=date&videoCategoryId=10&maxResults=3&videoEmbeddable=true&videoType=any&key=" + process.env.GATSBY_YT_KEY
 
-  const ytUrl = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=vinyl&publishedAfter=" + publishedAfter + "&publishedBefore=" + publishedBefore + "&type=video&order=date&videoCategoryId=10&maxResults=3&videoEmbeddable=true&videoType=any&key=" + process.env.GATSBY_YT_KEY
+  const basePlayerUrl = "https://www.youtube.com/watch?v="
 
+  let event = new Date
+
+  console.log(rDates)
   return (
     <main>
       <h1> Rhythm Roulette Virtual Edition</h1>
@@ -29,10 +30,15 @@ const IndexPage = () => {
             return (<div>Loading...</div>)
           }
           else if (response !== null) {
+
+            console.log(response.data.items)
             // Just mapping titles for now, need to look into iframes
             // as well as how best to randomly generate dates
             return (<div>{response.data.items.map((item) => {
-              return (<h1> {item.snippet.title}</h1>)
+              return (<>
+              <ReactPlayer url= {basePlayerUrl + item.id.videoId} />
+              <h1 key={item.snippet.title}> {item.snippet.title}</h1>
+              </> )
             })} <button onClick={() => makeRequest({ params: { refresh: true } })}>Refresh</button></div>)
           }
           return (<div>Default message before request is made.</div>)
