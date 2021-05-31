d438fc988abaa9983eb159b64889a410fb15279d
gauraklein <gaurakklein@gmail.com>
2021-05-30 21:26:58 -0500

    MVP done should be good to start making beats

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import VideoPlayer from "../components/video-player"
 
 import { Get } from 'react-axios'
 import ReactPlayer from 'react-player'
@@ -12,34 +13,36 @@ const IndexPage = () => {
 
   const ytUrl = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=vinyl&publishedAfter=" + rDates.publishedAfter + "&publishedBefore=" + rDates.publishedBefore + "&type=video&order=date&videoCategoryId=10&maxResults=3&videoEmbeddable=true&videoType=any&key=" + process.env.GATSBY_YT_KEY
 
-  const basePlayerUrl = "https://www.youtube.com/watch?v="
-
-  let event = new Date
-
+ 
   console.log(rDates)
   return (
-    <main>
-      <h1> Rhythm Roulette Virtual Edition</h1>
+    <main className="bg-white h-screen p-4 md:p-8 overflow-y-auto">
+      <h1 className="text-xl text-center m-4 font-bold"> Rhythm Roulette Virtual Edition</h1>
+
+      <p className="text-lg text-center m-4"> This project is based on
+      <br />
+      <a className="text-blue-600" href="https://www.youtube.com/playlist?list=PL_QcLOtFJOUgNxURr8B4lNtf_3e9fWZzl">
+       Rhythm Roulette by Mass Appeal.</a>
+       <br />
+      The basic steps are: 
+      <br />
+      1. Generate 3 videos 
+      <br />
+      2. Make a beat by sampling.
+      </p>
 
       <Get url={ytUrl}>
-        {(error, response, isLoading, makeRequest, axios) => {
+        {(error, response, isLoading, makeRequest) => {
           if (error) {
-            return (<div>Something bad happened: {error.message} <button onClick={() => makeRequest({ params: { reload: true } })}>Retry</button></div>)
+            return (<div>An unexpected error ocurred: {error.message} <button onClick={() => makeRequest({ params: { reload: true } })}>Retry</button></div>)
           }
           else if (isLoading) {
             return (<div>Loading...</div>)
           }
           else if (response !== null) {
 
-            console.log(response.data.items)
-            // Just mapping titles for now, need to look into iframes
-            // as well as how best to randomly generate dates
-            return (<div>{response.data.items.map((item) => {
-              return (<>
-              <ReactPlayer url= {basePlayerUrl + item.id.videoId} />
-              <h1 key={item.snippet.title}> {item.snippet.title}</h1>
-              </> )
-            })} <button onClick={() => makeRequest({ params: { refresh: true } })}>Refresh</button></div>)
+            return ( 
+            <VideoPlayer videoData={response.data.items} />)
           }
           return (<div>Default message before request is made.</div>)
         }}
