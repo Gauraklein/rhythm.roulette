2dd0488bfc22155f2dd9592a717c9ae8bd6cdc8e
gauraklein <gaurakklein@gmail.com>
2021-07-01 00:33:34 -0500

    Updates

--- gatsby-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  pathPrefix: "/rhythm.roulette",
-  siteMetadata: {
-    title: "rhythm.roulette",
-  },
-  plugins: [`gatsby-theme-material-ui`,
-            `gatsby-plugin-postcss`]
-};
-
